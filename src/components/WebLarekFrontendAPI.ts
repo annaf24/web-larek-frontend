0fe4e4296d48897d63a6d1f4364b0fdb321e5924
Anna Filippova <afilippova240@gmail.com>
2025-02-26 13:48:01 +0300

    настройка проекта

---
+++ src/components/WebLarekFrontendAPI.ts
@@ -0,0 +1,32 @@
+import { ICard, IOrder, IOrderSuccess } from "../types";
+import { Api, ApiListResponse } from "./base/api";
+
+export interface IWebLarekAPI {
+    getCardsAPI: () => Promise<ICard[]>;
+    addOrderAPI: (order: IOrder) => Promise<IOrderSuccess>;
+}
+
+export class WebLarekFrontendAPI extends Api{
+    readonly cdn: string;
+
+    constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+        super(baseUrl, options);
+        this.cdn = cdn;
+    }
+
+    getCardsAPI(): Promise<ICard[]> {
+        return this.get('/product').then((data: ApiListResponse<ICard>) => 
+            data.items.map((item) => ({
+                ...item,
+                image: this.cdn + item.image
+            }
+        ))
+    );
+    }
+    
+    addOrderAPI(order: IOrder): Promise<IOrderSuccess> {
+        return this.post('/order', order).then(
+            (data: IOrderSuccess) => data
+        );
+    }
+}
---
+++ src/types/index.ts
@@ -0,0 +1,70 @@
+// Карточка товара
+export interface ICard {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  category: string;
+  price: number | null;
+}
+
+export interface ICardWithSelection extends ICard {
+  isBasket: boolean;
+  isSelected: boolean;
+}
+
+// Каталог
+export interface ICatalogState {
+  cards: ICardWithSelection[];
+}
+
+export interface ICatalogActions {
+  setCards(data: ICard[]): void;
+  setCardSelected(id: string): void;
+  getCards(): ICardWithSelection[];
+  getCard(id: string): ICardWithSelection | undefined;
+  toggleCardInBasket(id: string): void;
+  getCardsBasket(): ICardWithSelection[];
+  getCardIdsBasket(): string[];
+  getBasketItemCount(): number;
+  getBasketTotalPrice(): number;
+  clearBasket(): void;
+  isCardInBasket(id: string): boolean;
+}
+
+// Пользователь
+export interface IUser {
+  payment?: string;
+  email?: string;
+  phone?: string;
+  address?: string;
+}
+
+export interface IUserState {
+  user: IUser;
+}
+
+export interface IUserActions {
+  setUserData(field: keyof IUser, value: string): void;
+  getUserData(): IUser;
+  clearUserData(): void;
+}
+
+// Заказ
+export interface IOrder extends IUser {
+  total: number;
+  items: string[];
+}
+
+export interface IOrderSuccess {
+  id: string;
+  total: number;
+}
+
+// Вспомогательные типы
+export type TCardCatalog = Omit<ICard, 'id' | 'description'>;
+export type TCardPreview = Omit<ICard, 'id'>;
+export type TCardBasket = Pick<ICard, 'title' | 'price'>;
+
+export type TUserOrder = Pick<IUser, 'payment' | 'address'>;
+export type TUserContacts = Pick<IUser, 'phone' | 'email'>;
