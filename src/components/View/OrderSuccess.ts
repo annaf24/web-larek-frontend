7c603583c66cc0289e65385fe337805a47aa0ac1
Anna Filippova <afilippova240@gmail.com>
2025-02-28 11:44:16 +0300

    добавлено окноуспешной оплаты

---
+++ src/components/View/OrderSuccess.ts
@@ -0,0 +1,30 @@
+import { IOrderSuccess } from '../../types';
+import { ensureElement } from '../../utils/utils';
+import { Component } from '../base/components';
+import { IEvents } from '../base/events';
+
+export class Success extends Component<IOrderSuccess> {
+  protected _total: HTMLElement;
+  protected button: HTMLButtonElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+
+    // Находим инициализированные элементы в DOM
+    this._total = ensureElement<HTMLElement>('.order-success__description', this.container);
+    this.button = ensureElement<HTMLButtonElement>('.order-success__close', this.container);
+
+    // Добавляем обработчик клика для закрытия окна успеха
+    this.button.addEventListener('click', this.handleClose);
+  }
+
+  // Геттер для установки отображаемой суммы списанных синапсов
+  set total(value: number) {
+    this.setText(this._total, `Списано ${value} синапсов`);
+  }
+
+  // Обработчик закрытия окна успеха, который передает событие в EventEmitter
+  private handleClose = (): void => {
+    this.events.emit('successView:close');
+  };
+}
